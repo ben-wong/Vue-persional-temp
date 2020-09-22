50f6abcecb2fdb3c684c882b76f21e59303f7931
huangjunzsu <huangjunzsu@gmail.com>
2020-09-22 14:19:57 +0800

    Create AnnualTotalViewV2.vue

---
+++ src/components/common/AnnualTotalViewV2.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="annual-total">
+  <span>{{ title }}：</span>
+  <span class="annual-total-num">{{ annualTotalNum }}{{ annualTotalNum === '-' ? '' : unit }}</span>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'AnnualTotalV2',
+  props: {
+    title: {
+      type: String,
+      required: false,
+      default: '-'
+    },
+    annualTotalNum: {
+      type: String,
+      required: false,
+      default: '-'
+    },
+    unit: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+  methods: {
+  }
+};
+</script>
+
+<style scoped lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+}
+.annual-total{
+  min-width: 300px;
+  // padding: 16px 0 16px 0;
+  display: flex;
+  align-items: center;
+}
+.annual-total-num {
+  font-size: 32px;
+  color: #00AAEE;
+  padding: 16px 0 16px 0;
+}
+</style>
