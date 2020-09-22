48963c40b64b3e9b081e360d6cd2badb19002500
huangjunzsu <huangjunzsu@gmail.com>
2020-09-22 14:23:17 +0800

    Create PageHeaderView.vue

---
+++ src/components/common/PageHeaderView.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-page-header @back="goBack" content="详情页面">
+  </el-page-header>
+</template>
+
+<script>
+export default {
+  methods: {
+    goBack() {
+      console.log('go back');
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+  .el-page-header {
+    background: #fff;
+    margin: 6px;
+    border-radius: 6%;
+  }
+  .el-menu {
+    text-align: start
+  }
+</style>
